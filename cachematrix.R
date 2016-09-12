c02bff063939e3c549338110ec32daa10b4ca9de
Benjamin-Mathew <benjaminvmathew@hotmail.com>
2016-09-13 00:11:10 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be 
+## some benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Two functions have been written to help in caching the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" returned by 
+## the above function makeCacheMatrix. If the inverse has already been calculated, 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+    if(!is.null(i)) {
+        message("getting cached data.")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data)
+    x$setinv(i)
+    i
 }
+
+##Sample Output
+##> x <- matrix(1:4,2,2)
+## > x
+##       [,1] [,2]
+## [1,]    1    3
+## [2,]    2    4
+
+## > x_inv <- makeCacheMatrix(x)
+## > x_inv$get()
+##      [,1] [,2]
+## [1,]    1    3
+## [2,]    2    4
+
+## First run - no cache
+## > cacheSolve(x_inv)
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
+## Second run - Retrieving cache
+## > cacheSolve(x_inv)
+## getting cached data.
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## >
+
+
